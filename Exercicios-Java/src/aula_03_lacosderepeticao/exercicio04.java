7ab210c5fb0a8c76863c9468bad7c98a519dcc64
Laise France <laise94@hotmail.com>
2023-02-10 20:18:12 -0300

    add exercicio 05 do...while (num positivos)

--- Exercicios-Java/src/aula_03_lacosderepeticao/exercicio04.java
+++ Exercicios-Java/src/aula_03_lacosderepeticao/exercicio04.java
@@ -3,30 +3,5 @@ package aula_03_lacosderepeticao;
 import java.util.Scanner;
 
 public class exercicio04 {
-	public class SomaMultiplos3 {
-		  public static void main(String[] args) {
-		    Scanner input = new Scanner(System.in);
-		    int number, total = 0, count = 0;
-		    
-		    System.out.print("Digite um número: ");
-		    number = input.nextInt();
-		    
-		    while (number != 0) {
-		      if (number % 3 == 0) {
-		        total = total + number;
-		        count = count + 1;
-		      }
-		      System.out.print("Digite um número: ");
-		      number = input.nextInt();
-		    }
-		    
-		    if (count > 0) {
-		      System.out.println("A média de todos os números múltiplos de 3 é: " + (total / count));
-		    } else {
-		      System.out.println("Não existem números múltiplos de 3");
-		    }
-		    
-		    input.close();
-		  }
-		}
+	
 }
