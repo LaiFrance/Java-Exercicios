515ef690dc06124d9fa7e83e5bd0770c16a5f29d
Laise France <laise94@hotmail.com>
2023-02-10 20:21:23 -0300

    ajustando

--- Exercicios-Java/src/aula_03_lacosderepeticao/doWhile.java
+++ Exercicios-Java/src/aula_03_lacosderepeticao/doWhile.java
@@ -3,20 +3,19 @@ package aula_03_lacosderepeticao;
 import java.util.Scanner;
 
 public class doWhile {
-	 public static void main(String[] args) {
+	public static void main(String[] args) {
 
-	        int numero, resultado, contador = 3;
-	        Scanner leia = new Scanner(System.in);
+		int numero, resultado, contador = 3;
+		Scanner leia = new Scanner(System.in);
 
-	        do {
-	            System.out.println("\nDigite um número inteiro: ");
-	            numero = leia.nextInt();
+		do {
+			System.out.println("\nDigite um número inteiro: ");
+			numero = leia.nextInt();
 
-	            resultado = numero * 5;
+			resultado = numero * 5;
 
-	            System.out.println("\nO resultado da multiplicação é: " + resultado);
+			System.out.println("\nO resultado da multiplicação é: " + resultado);
 
-	        } while (contador <= 2);
-	    }
+		} while (contador <= 2);
 	}
-
+}
