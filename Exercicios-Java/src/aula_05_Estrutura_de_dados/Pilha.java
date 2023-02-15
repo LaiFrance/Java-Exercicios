7098efc8ab862edbe88ba1db2a715372e5ca1bf5
Laise France <laise94@hotmail.com>
2023-02-15 15:13:33 -0300

    add exercicio01_Fila

--- Exercicios-Java/src/aula_05_Estrutura_de_dados/Pilha.java
+++ Exercicios-Java/src/aula_05_Estrutura_de_dados/Pilha.java
@@ -1,5 +1,9 @@
 package aula_05_Estrutura_de_dados;
 
 public class Pilha {
+	public static void main() {
+		
+		
+	}
 
 }
---
+++ Exercicios-Java/src/aula_05_Estrutura_de_dados/exercicio01.java
@@ -0,0 +1,49 @@
+package aula_05_Estrutura_de_dados;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class exercicio01 {
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		Queue<String> fila = new LinkedList<String>();
+
+		int opcao = -1;
+		while (opcao != 0) {
+			System.out.println("\nMenu:");
+			System.out.println("1-Adicionar Cliente na Fila");
+			System.out.println("2-Listar todos os clientes");
+			System.out.println("3-Retirar Cliente da Fila");
+			System.out.println("0-Sair");
+			System.out.print("Entre com a opção desejada: ");
+			opcao = input.nextInt();
+
+			if (opcao == 1) {
+				System.out.print("Digite o nome: ");
+				String nome = input.next();
+				fila.add(nome);
+				System.out.println("Fila:");
+				System.out.println("Cliente adicionado!");
+			} else if (opcao == 2) {
+				System.out.println("Fila:");
+				for (String nome : fila) {
+					System.out.println("Fila:");
+					System.out.println(nome);
+				}
+			} else if (opcao == 3) {
+				if (fila.isEmpty()) {
+					System.out.println("A Fila está vazia!");
+				} else {
+					String nome = fila.poll();
+					System.out.println("Fila:");
+					System.out.println("Cliente retirado: " + nome);
+				}
+			}
+		}
+
+		System.out.println("Programa finalizado!");
+	}
+
+}
