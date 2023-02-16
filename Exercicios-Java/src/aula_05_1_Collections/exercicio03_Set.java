fae3d3654eccbdcd6348de2bf3b4d4b6dcfe4488
Laise France <laise94@hotmail.com>
2023-02-16 11:08:25 -0300

    add aula06_metodos

--- Exercicios-Java/src/aula_05_1_Collections/exercicio03_Set.java
+++ Exercicios-Java/src/aula_05_1_Collections/exercicio03_Set.java
@@ -6,20 +6,20 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class exercicio03_Set {
-	public static void main(String[] args) { 
-        Set<Integer> set = new HashSet<Integer>();
-        Scanner input = new Scanner(System.in); 
-        int value; 
-        System.out.println("Digite 10 valores inteiros não repetidos:");
-        for(int i = 0; i < 10; i++) {
-            value = input.nextInt();
-            set.add(value);
-        }
-        //Mostrando os dados do Set
-        System.out.println("\nListar dados do Set:");
-        Iterator<Integer> it = set.iterator();
-        while(it.hasNext()) {
-            System.out.println(it.next());
-        }
-  } 
+	public static void main(String[] args) {
+		Set<Integer> set = new HashSet<Integer>();
+		Scanner input = new Scanner(System.in);
+		int value;
+		System.out.println("Digite 10 valores inteiros não repetidos:");
+		for (int i = 0; i < 10; i++) {
+			value = input.nextInt();
+			set.add(value);
+		}
+		// Mostrando os dados do Set
+		System.out.println("\nListar dados do Set:");
+		Iterator<Integer> it = set.iterator();
+		while (it.hasNext()) {
+			System.out.println(it.next());
+		}
+	}
 }
---
+++ Exercicios-Java/src/aula_06_metodos/TestaMetodos.java
@@ -0,0 +1,10 @@
+package aula_06_metodos;
+
+public class TestaMetodos {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
