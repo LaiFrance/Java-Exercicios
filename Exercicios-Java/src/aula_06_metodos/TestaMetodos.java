363c646e139cd4933886d78e59eecdb1793b1bf7
Laise France <laise94@hotmail.com>
2023-02-16 11:29:41 -0300

    add Testametodo_aula06_metodos

--- Exercicios-Java/src/aula_06_metodos/TestaMetodos.java
+++ Exercicios-Java/src/aula_06_metodos/TestaMetodos.java
@@ -4,7 +4,31 @@ public class TestaMetodos {
 
 	public static void main(String[] args) {
 		
-
+		metodoPublic();
+		metodoPadrao();
+		metodoProtegido();
+		metodoPrivado();
+		System.out.println(soma(2, 4));
 	}
 
+	public static void metodoPublic() {
+		System.out.println("Eu sou um Método Público!");
+	}
+	
+	public static int soma(int numero1, int numero2) {
+		return numero1 + numero2;
+	}
+	
+	static void metodoPadrao() {
+		System.out.println("Eu sou um Método Friendly!");
+	}
+	
+	protected static void metodoProtegido() {
+		System.out.println("Eu sou um Método Protegido!");
+	}
+	
+	private static void metodoPrivado() {
+		System.out.println("Eu sou um Método Privado!");
+	}
+	
 }
