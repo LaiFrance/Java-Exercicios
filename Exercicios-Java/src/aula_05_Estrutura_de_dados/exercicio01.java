f1a0f57a576b10b37e57d2866d31d055006f1528
Laise France <laise94@hotmail.com>
2023-02-15 21:33:02 -0300

    add exercicio02_Pilha

--- Exercicios-Java/src/aula_05_Estrutura_de_dados/exercicio01.java
+++ Exercicios-Java/src/aula_05_Estrutura_de_dados/exercicio01.java
@@ -7,42 +7,40 @@ import java.util.Scanner;
 public class exercicio01 {
 	public static void main(String[] args) {
 
-		Scanner input = new Scanner(System.in);
-		Queue<String> fila = new LinkedList<String>();
+		try (Scanner leia = new Scanner(System.in)) {
+			Queue<String> fila = new LinkedList<String>();
 
-		int opcao = -1;
-		while (opcao != 0) {
-			System.out.println("\nMenu:");
-			System.out.println("1-Adicionar Cliente na Fila");
-			System.out.println("2-Listar todos os clientes");
-			System.out.println("3-Retirar Cliente da Fila");
-			System.out.println("0-Sair");
-			System.out.print("Entre com a opção desejada: ");
-			opcao = input.nextInt();
+			int opcao = -1;
+			while (opcao != 0) {
+				System.out.println("\nMenu:");
+				System.out.println("1-Adicionar Cliente na Fila");
+				System.out.println("2-Listar todos os clientes");
+				System.out.println("3-Retirar Cliente da Fila");
+				System.out.println("0-Sair");
+				System.out.print("Entre com a opção desejada: ");
+				opcao = leia.nextInt();
 
-			if (opcao == 1) {
-				System.out.print("Digite o nome: ");
-				String nome = input.next();
-				fila.add(nome);
-				System.out.println("Fila:");
-				System.out.println("Cliente adicionado!");
-			} else if (opcao == 2) {
-				System.out.println("Fila:");
-				for (String nome : fila) {
+				if (opcao == 1) {
+					System.out.print("Digite o nome: ");
+					String nome = leia.next();
+					fila.add(nome);
 					System.out.println("Fila:");
-					System.out.println(nome);
-				}
-			} else if (opcao == 3) {
-				if (fila.isEmpty()) {
-					System.out.println("A Fila está vazia!");
-				} else {
-					String nome = fila.poll();
-					System.out.println("Fila:");
-					System.out.println("Cliente retirado: " + nome);
+					System.out.println("Cliente adicionado!");
+				} else if (opcao == 2) {
+					for (String nome : fila) {
+						System.out.println(nome);
+					}
+				} else if (opcao == 3) {
+					if (fila.isEmpty()) {
+						System.out.println("A Fila está vazia!");
+					} else {
+						String nome = fila.poll();
+						System.out.println("Fila:");
+						System.out.println("Cliente retirado: " + nome);
+					}
 				}
 			}
 		}
-
 		System.out.println("Programa finalizado!");
 	}
 
--- Exercicios-Java/src/aula_05_Estrutura_de_dados/exercicio02.java
+++ Exercicios-Java/src/aula_05_Estrutura_de_dados/exercicio02.java
@@ -1,5 +1,50 @@
 package aula_05_Estrutura_de_dados;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class exercicio02 {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		int opcao;
+		Stack<String> pilhaLivros = new Stack<String>();
+
+		do {
+			System.out.println("\nMenu: ");
+			System.out.println("\n1-Adicionar Livro na pilha");
+			System.out.println("\n2-Listar todos os Livros");
+			System.out.println("\n3-Retirar Livro da pilha");
+			System.out.println("\n0-Sair");
+			System.out.print("\nEntre com a opção desejada: ");
+			opcao = entrada.nextInt();
+
+			switch (opcao) {
+			case 1:
+				System.out.print("Digite o nome: ");
+				String nomeLivro = entrada.next();
+				pilhaLivros.push(nomeLivro);
+				System.out.println("Livro adicionado!");
+				break;
+			case 2:
+				System.out.println("Pilha: ");
+				for (String livro : pilhaLivros) {
+					System.out.println(livro);
+				}
+				break;
+			case 3:
+				if (!pilhaLivros.empty()) {
+					System.out.println("Livro retirado:");
+					System.out.println(pilhaLivros.pop());
+				} else {
+					System.out.println("Pilha vazia!");
+				}
+				break;
+			case 0:
+				break;
+			default:
+				System.out.println("Opção Inválida!");
+			}
+		} while (opcao != 0);
+	}
 
 }
